e57e3f2b3840cc361a7c7b92b3e78d5b1dab3630
Yuichiro Koura <y-koura@MBP026.local>
2023-04-13 09:45:08 +0900

    serchbook endpoint

--- src/books/books.controller.ts
+++ src/books/books.controller.ts
@@ -1,5 +1,5 @@
-import { Controller, Get } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
+import { Controller, Get, Query } from '@nestjs/common';
+import { Book, PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
@@ -8,6 +8,18 @@ export class BooksController {
   @Get('/')
   async getBooks() {
     const books = await prisma.book.findMany();
+    return books;
+  }
+  @Get('/search')
+  async searchBooks(@Query('title') title: string): Promise<Book[]> {
+    const books = await prisma.book.findMany({
+      where: {
+        title: {
+          contains: title,
+        },
+      },
+    });
+
     return books;
   }
 }
